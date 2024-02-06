0920885fb11a857537484821f90e95b12b15195c
Paulo Renato <108829437+seven-renato@users.noreply.github.com>
2024-02-05 23:25:58 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {Soma(numero1,numero2)}");
+            
+            Console.WriteLine("Aperte Enter para finalizar");
+
+            Console.ReadLine();
+
+
+        }
+
+        public static int Soma( int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int resultado = Soma(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            MessageBox.Show($"O resultado da soma foi: {resultado}");
+        }
+
+        public static int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
